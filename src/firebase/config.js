8f4322bbf7ab6fd8fdb0b328567ea2faf28e004c
Ashank16 <ashankjuyal@gmail.com>
2020-10-01 13:36:10 +0530

    Configuring Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyAkNqlC8SOCu8xdBheJeKgw-QmkybtQrjE",
+    authDomain: "confusionserver-959ae.firebaseapp.com",
+    databaseURL: "https://confusionserver-959ae.firebaseio.com",
+    projectId: "confusionserver-959ae",
+    storageBucket: "confusionserver-959ae.appspot.com",
+    messagingSenderId: "856878697981",
+    appId: "1:856878697981:web:0b479789fab7ddc9989d94",
+    measurementId: "G-FXSN0Q7719"
+};
